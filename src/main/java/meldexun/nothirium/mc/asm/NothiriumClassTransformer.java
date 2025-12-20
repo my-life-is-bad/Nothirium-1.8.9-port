4f4dfe39e8bdebcb96fbee709a3e5a5c3f149640
my-life-is-bad <abyan68shamsi@gmail.com>
2025-12-20 20:26:16 +0500

    fixing classtransformer

--- src/main/java/meldexun/nothirium/mc/asm/NothiriumClassTransformer.java
+++ src/main/java/meldexun/nothirium/mc/asm/NothiriumClassTransformer.java
@@ -15,6 +15,8 @@ import java.util.zip.ZipFile;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.VarInsnNode;
 import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.JumpInsnNode;
 import org.objectweb.asm.tree.LabelNode;
@@ -72,48 +74,60 @@ public class NothiriumClassTransformer extends HashMapClassNodeClassTransformer 
 	@Override
 	protected void registerTransformers(IClassTransformerRegistry registry) {
 		// @formatter:off
-		// registry.addObf("net.minecraft.client.renderer.RenderGlobal", "setWorldAndLoadRenderers", "func_72732_a", "(Lnet/minecraft/client/multiplayer/WorldClient;)V", ClassWriter.COMPUTE_FRAMES, methodNode -> {
-		// 	AbstractInsnNode targetNode1 = ASMUtil.first(methodNode).opcode(Opcodes.INVOKEINTERFACE).methodInsn("java/util/Set", "clear", "()V").find();
-		// 	targetNode1 = ASMUtil.prev(methodNode, targetNode1).type(LabelNode.class).find();
+		registry.addObf("net.minecraft.client.renderer.RenderGlobal", "setWorldAndLoadRenderers", "func_72732_a", "(Lnet/minecraft/client/multiplayer/WorldClient;)V", ClassWriter.COMPUTE_FRAMES, methodNode -> {
+			LabelNode skipLabel = new LabelNode();
+			InsnList insns = new InsnList();
 
-		// 	AbstractInsnNode popNode1 = ASMUtil.last(methodNode).opcode(Opcodes.PUTFIELD).fieldInsnObf("net/minecraft/client/renderer/RenderGlobal", "renderDispatcher", "field_174995_M", "Lnet/minecraft/client/renderer/chunk/ChunkRenderDispatcher;").find();
-		// 	popNode1 = ASMUtil.next(methodNode, popNode1).type(LabelNode.class).find();
+			insns.add(new VarInsnNode(Opcodes.ALOAD, 1));
 
-		// 	methodNode.instructions.insert(targetNode1, ASMUtil.listOf(
-		// 		new MethodInsnNode(Opcodes.INVOKESTATIC, "meldexun/nothirium/mc/renderer/ChunkRenderManager", "dispose", "()V", false),
-		// 		new JumpInsnNode(Opcodes.GOTO, (LabelNode) popNode1)
-		// 	));
-		// });
+			insns.add(new JumpInsnNode(Opcodes.IFNONNULL, skipLabel));
 
-		// registry.addObf("net.minecraft.client.renderer.RenderGlobal", "loadRenderers", "func_72712_a", "()V", ClassWriter.COMPUTE_FRAMES, methodNode -> {
-		// 	AbstractInsnNode targetNode1 = ASMUtil.first(methodNode).opcode(Opcodes.INVOKESPECIAL).methodInsn("net/minecraft/client/renderer/chunk/ChunkRenderDispatcher", "<init>", "()V").find();
-		// 	targetNode1 = ASMUtil.prev(methodNode, targetNode1).type(JumpInsnNode.class).find();
-		// 	targetNode1 = ASMUtil.prev(methodNode, targetNode1).type(LabelNode.class).find();
-		// 	AbstractInsnNode popNode1 = ASMUtil.next(methodNode, targetNode1).opcode(Opcodes.INVOKESPECIAL).methodInsn("net/minecraft/client/renderer/chunk/ChunkRenderDispatcher", "<init>", "()V").find(); 
-		// 	popNode1 = ASMUtil.next(methodNode, popNode1).type(LabelNode.class).find();
+			insns.add(new MethodInsnNode(
+				Opcodes.INVOKESTATIC,
+				"meldexun/nothirium/mc/renderer/ChunkRenderManager",
+				"dispose",
+				"()V",
+				false
+			));
+			insns.add(skipLabel);
 
-		// 	AbstractInsnNode targetNode2 = ASMUtil.next(methodNode, popNode1).methodInsnObf("net/minecraft/client/renderer/RenderGlobal", "generateSky2", "func_174964_o", "()V").find();
-		// 	targetNode2 = ASMUtil.next(methodNode, targetNode2).type(LabelNode.class).find();
-		// 	AbstractInsnNode popNode2 = ASMUtil.last(methodNode).opcode(Opcodes.PUTFIELD).fieldInsnObf("net/minecraft/client/renderer/RenderGlobal", "renderEntitiesStartupCounter", "field_72740_G", "I").find(); 
-		// 	popNode2 = ASMUtil.next(methodNode, popNode2).type(LabelNode.class).find();
+			methodNode.instructions.insertBefore(
+				methodNode.instructions.getFirst(),
+				insns
+			);
 
-		// 	methodNode.instructions.insert(targetNode1, ASMUtil.listOf(
-		// 		new JumpInsnNode(Opcodes.GOTO, (LabelNode) popNode1)
-		// 	));
+		});
 
-		// 	methodNode.instructions.insert(targetNode2, ASMUtil.listOf(
-		// 		new MethodInsnNode(Opcodes.INVOKESTATIC, "meldexun/nothirium/mc/renderer/ChunkRenderManager", "allChanged", "()V", false),
-		// 		new JumpInsnNode(Opcodes.GOTO, (LabelNode) popNode2)
-		// 	));
-		// });
-		registry.addObf("net.minecraft.client.renderer.RenderGlobal", "setWorldAndLoadRenderers", "func_72732_a", "(Lnet/minecraft/client/multiplayer/WorldClient;)V", 2, methodNode -> {
-            AbstractInsnNode firstInsn = methodNode.instructions.getFirst();
-            methodNode.instructions.insertBefore(firstInsn, ASMUtil.listOf((AbstractInsnNode)new MethodInsnNode(184, "meldexun/nothirium/mc/renderer/ChunkRenderManager", "dispose", "()V", false)));
-        });
-        registry.addObf("net.minecraft.client.renderer.RenderGlobal", "loadRenderers", "func_72712_a", "()V", 2, methodNode -> {
-            AbstractInsnNode firstInsn = methodNode.instructions.getFirst();
-            methodNode.instructions.insertBefore(firstInsn, ASMUtil.listOf((AbstractInsnNode)new MethodInsnNode(184, "meldexun/nothirium/mc/renderer/ChunkRenderManager", "allChanged", "()V", false)));
-        });
+		registry.addObf("net.minecraft.client.renderer.RenderGlobal", "loadRenderers", "func_72712_a", "()V", ClassWriter.COMPUTE_FRAMES, methodNode -> {
+			// AbstractInsnNode targetNode1 = ASMUtil.first(methodNode).opcode(Opcodes.INVOKESPECIAL).methodInsn("net/minecraft/client/renderer/chunk/ChunkRenderDispatcher", "<init>", "()V").find();
+			// targetNode1 = ASMUtil.prev(methodNode, targetNode1).type(JumpInsnNode.class).find();
+			// targetNode1 = ASMUtil.prev(methodNode, targetNode1).type(LabelNode.class).find();
+			// AbstractInsnNode popNode1 = ASMUtil.next(methodNode, targetNode1).opcode(Opcodes.INVOKESPECIAL).methodInsn("net/minecraft/client/renderer/chunk/ChunkRenderDispatcher", "<init>", "()V").find(); 
+			// popNode1 = ASMUtil.next(methodNode, popNode1).type(LabelNode.class).find();
+
+			AbstractInsnNode targetNode2 = ASMUtil.first(methodNode).methodInsnObf("net/minecraft/client/renderer/RenderGlobal", "generateSky2", "func_174964_o", "()V").find();
+			targetNode2 = ASMUtil.next(methodNode, targetNode2).type(LabelNode.class).find();
+			AbstractInsnNode popNode2 = ASMUtil.last(methodNode).opcode(Opcodes.PUTFIELD).fieldInsnObf("net/minecraft/client/renderer/RenderGlobal", "renderEntitiesStartupCounter", "field_72740_G", "I").find(); 
+			popNode2 = ASMUtil.next(methodNode, popNode2).type(LabelNode.class).find();
+
+			// methodNode.instructions.insert(targetNode1, ASMUtil.listOf(
+			// 	new JumpInsnNode(Opcodes.GOTO, (LabelNode) popNode1)
+			// ));
+
+			methodNode.instructions.insert(targetNode2, ASMUtil.listOf(
+				new MethodInsnNode(Opcodes.INVOKESTATIC, "meldexun/nothirium/mc/renderer/ChunkRenderManager", "allChanged", "()V", false),
+				new JumpInsnNode(Opcodes.GOTO, (LabelNode) popNode2)
+			));
+		});
+		// registry.addObf("net.minecraft.client.renderer.RenderGlobal", "setWorldAndLoadRenderers", "func_72732_a", "(Lnet/minecraft/client/multiplayer/WorldClient;)V", 2, methodNode -> {
+        //     AbstractInsnNode firstInsn = methodNode.instructions.getFirst();
+        //     methodNode.instructions.insertBefore(firstInsn, ASMUtil.listOf((AbstractInsnNode)new MethodInsnNode(184, "meldexun/nothirium/mc/renderer/ChunkRenderManager", "dispose", "()V", false)));
+        // });
+
+        // registry.addObf("net.minecraft.client.renderer.RenderGlobal", "loadRenderers", "func_72712_a", "()V", 2, methodNode -> {
+        //     AbstractInsnNode firstInsn = methodNode.instructions.getFirst();
+        //     methodNode.instructions.insertBefore(firstInsn, ASMUtil.listOf((AbstractInsnNode)new MethodInsnNode(184, "meldexun/nothirium/mc/renderer/ChunkRenderManager", "allChanged", "()V", false)));
+        // });
 		// @formatter:on
 
 		Map<Path, ZipFile> zipCache = new HashMap<>();
--- src/main/java/meldexun/nothirium/mc/renderer/chunk/SectionRenderCache.java
+++ src/main/java/meldexun/nothirium/mc/renderer/chunk/SectionRenderCache.java
@@ -101,6 +101,7 @@ public class SectionRenderCache implements IBlockAccess {
 	@Nullable
 	public TileEntity getTileEntity(BlockPos pos) {
 		Chunk chunk = this.getChunk(pos);
+		System.out.println("TileEntity is valid?" + (chunk.getTileEntity(pos, EnumCreateEntityType.CHECK) == null ? "false" : "true"));
 		return chunk == null ? null : chunk.getTileEntity(pos, EnumCreateEntityType.CHECK);
 	}
 
