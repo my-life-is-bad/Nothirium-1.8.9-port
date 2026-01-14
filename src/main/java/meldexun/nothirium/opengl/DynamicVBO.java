e7ab8742e0c133eb304afcb0cf75a3caf6ba0dc8
my-life-is-bad <abyan68shamsi@gmail.com>
2026-01-15 01:25:07 +0500

    Version 0.4.8.3

--- src/main/java/meldexun/nothirium/opengl/DynamicVBO.java
+++ src/main/java/meldexun/nothirium/opengl/DynamicVBO.java
@@ -4,130 +4,161 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.lwjgl.opengl.ARBBufferStorage;
+import org.lwjgl.opengl.ARBDirectStateAccess;
 import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL44;
+import org.lwjgl.opengl.GL45;
 
 import meldexun.nothirium.api.renderer.IVBOPart;
 import meldexun.nothirium.util.SectorizedList;
 import meldexun.nothirium.util.SectorizedList.Sector;
 import meldexun.nothirium.util.math.MathUtil;
+import meldexun.renderlib.util.GLUtil;
 
 public class DynamicVBO {
 
-	private final int vertexSize;	// vertexSize -> nextOffset??
-	private final int vertexCountPerSector;
-	private final int sectorSize;
-	private final SectorizedList sectors;
-	private int vbo;
-	private final List<Runnable> listeners = new ArrayList<>();
+    private final int vertexSize;	// vertexSize -> nextOffset??
+    private final int vertexCountPerSector;
+    private final int sectorSize;
+    private final SectorizedList sectors;
+    private int vbo;
+    private final List<Runnable> listeners = new ArrayList<>();
 
-	public DynamicVBO(int vertexSize, int vertexCountPerSector, int sectorCount) {
-		this.vertexSize = vertexSize;
-		this.vertexCountPerSector = vertexCountPerSector;
-		this.sectorSize = vertexCountPerSector * vertexSize;
-		this.sectors = new SectorizedList(sectorCount) {
-			@Override
-			protected void grow(int minContinousSector) {
-				int oldSectorCount = this.getSectorCount();
-				super.grow(minContinousSector);
+    public DynamicVBO(int vertexSize, int vertexCountPerSector, int sectorCount) {
+        this.vertexSize = vertexSize;
+        this.vertexCountPerSector = vertexCountPerSector;
+        this.sectorSize = vertexCountPerSector * vertexSize;
+        this.sectors = new SectorizedList(sectorCount) {
+            @Override
+            protected void grow(int minContinousSector) {
+                int oldSectorCount = this.getSectorCount();
+                super.grow(minContinousSector);
 
-				int newVbo = GLHelper.growBuffer(vbo, (long) sectorSize * oldSectorCount,
-						(long) sectorSize * getSectorCount());
-				if (newVbo != vbo) {
-					vbo = newVbo;
-					listeners.forEach(Runnable::run);
-				}
-			}
-		};
-		this.vbo = GL15.glGenBuffers();
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo);
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, (long) sectorSize * sectorCount, GL15.GL_STREAM_DRAW);
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-	}
+                int newVbo = GLHelper.growBuffer(vbo, (long) sectorSize * oldSectorCount,
+                        (long) sectorSize * getSectorCount());
+                if (newVbo != vbo) {
+                    vbo = newVbo;
+                    listeners.forEach(Runnable::run);
+                }
+            }
+        };
+        long size = (long) sectorSize * sectorCount;
+        if (GLUtil.CAPS.OpenGL45) {
+            this.vbo = GL45.glCreateBuffers();
+            GL45.glNamedBufferStorage(this.vbo, size, GL44.GL_DYNAMIC_STORAGE_BIT);
+        } else if (GLUtil.CAPS.GL_ARB_direct_state_access) {
+            this.vbo = ARBDirectStateAccess.glCreateBuffers();
+            ARBDirectStateAccess.glNamedBufferStorage(this.vbo, size, GL44.GL_DYNAMIC_STORAGE_BIT);
+        } else if (GLUtil.CAPS.OpenGL44) {
+            this.vbo = GL15.glGenBuffers();
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vbo);
+            GL44.glBufferStorage(GL15.GL_ARRAY_BUFFER, size, GL44.GL_DYNAMIC_STORAGE_BIT);
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+        } else if (GLUtil.CAPS.GL_ARB_buffer_storage) {
+            this.vbo = GL15.glGenBuffers();
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vbo);
+            ARBBufferStorage.glBufferStorage(GL15.GL_ARRAY_BUFFER, size, ARBBufferStorage.GL_DYNAMIC_STORAGE_BIT);
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+        } else {
+            this.vbo = GL15.glGenBuffers();
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vbo);
+            GL15.glBufferData(GL15.GL_ARRAY_BUFFER, size, GL15.GL_DYNAMIC_COPY);
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+        }
+    }
 
-	public void addListener(Runnable listener) {
-		this.listeners.add(listener);
-	}
+    public void addListener(Runnable listener) {
+        this.listeners.add(listener);
+    }
 
-	public void removeListener(Runnable listener) {
-		this.listeners.remove(listener);
-	}
+    public void removeListener(Runnable listener) {
+        this.listeners.remove(listener);
+    }
 
-	public VBOPart buffer(ByteBuffer data) {
-		int size = data.limit();
-		int requiredSectors = MathUtil.ceilDiv(size, this.sectorSize);
-		if (requiredSectors <= 0) {
-			throw new IllegalArgumentException();
-		}
-		Sector sector = this.sectors.claim(requiredSectors);
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo);
-		GL15.glBufferSubData(GL15.GL_ARRAY_BUFFER, (long) sectorSize * sector.getFirstSector(), data);
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-		return new VBOPart(sector, size / this.vertexSize);
-	}
+    public VBOPart buffer(ByteBuffer data) {
+        int size = data.remaining();
+        int requiredSectors = MathUtil.ceilDiv(size, this.sectorSize);
+        if (requiredSectors <= 0) {
+            throw new IllegalArgumentException();
+        }
+        Sector sector = this.sectors.claim(requiredSectors);
+        long offset = (long) sectorSize * sector.getFirstSector();
+        if (GLUtil.CAPS.OpenGL45) {
+            GL45.glNamedBufferSubData(this.vbo, offset, data);
+        } else if (GLUtil.CAPS.GL_ARB_direct_state_access) {
+            ARBDirectStateAccess.glNamedBufferSubData(this.vbo, offset, data);
+        } else {
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vbo);
+            GL15.glBufferSubData(GL15.GL_ARRAY_BUFFER, offset, data);
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+        }
+        return new VBOPart(sector, size / this.vertexSize);
+    }
 
-	public void free(Sector sector) {
-		this.sectors.free(sector);
-	}
+    public void free(Sector sector) {
+        this.sectors.free(sector);
+    }
 
-	public int getVbo() {
-		return this.vbo;
-	}
+    public int getVbo() {
+        return this.vbo;
+    }
 
-	public void dispose() {
-		GL15.glDeleteBuffers(this.vbo);
-	}
+    public void dispose() {
+        GL15.glDeleteBuffers(this.vbo);
+    }
 
-	public class VBOPart implements IVBOPart {
+    public class VBOPart implements IVBOPart {
 
-		private final Sector sector;
-		private final int vertexFirst;
-		private final int vertexCount;
-		private boolean valid = true;
+        private final Sector sector;
+        private final int vertexFirst;
+        private final int vertexCount;
+        private boolean valid = true;
 
-		private VBOPart(Sector sector, int vertexCount) {
-			this.sector = sector;
-			this.vertexFirst = sector.getFirstSector() * vertexCountPerSector;
-			this.vertexCount = vertexCount;
-		}
+        private VBOPart(Sector sector, int vertexCount) {
+            this.sector = sector;
+            this.vertexFirst = sector.getFirstSector() * vertexCountPerSector;
+            this.vertexCount = vertexCount;
+        }
 
-		@Override
-		public int getVBO() {
-			return DynamicVBO.this.vbo;
-		}
+        @Override
+        public int getVBO() {
+            return DynamicVBO.this.vbo;
+        }
 
-		@Override
-		public int getFirst() {
-			return this.vertexFirst;
-		}
+        @Override
+        public int getFirst() {
+            return this.vertexFirst;
+        }
 
-		@Override
-		public int getCount() {
-			return this.vertexCount;
-		}
+        @Override
+        public int getCount() {
+            return this.vertexCount;
+        }
 
-		@Override
-		public int getOffset() {
-			return this.vertexFirst * DynamicVBO.this.vertexSize;
-		}
+        @Override
+        public int getOffset() {
+            return this.vertexFirst * DynamicVBO.this.vertexSize;
+        }
 
-		@Override
-		public int getSize() {
-			return this.vertexCount * DynamicVBO.this.vertexSize;
-		}
+        @Override
+        public int getSize() {
+            return this.vertexCount * DynamicVBO.this.vertexSize;
+        }
 
-		@Override
-		public void free() {
-			if (this.valid) {
-				DynamicVBO.this.free(this.sector);
-				this.valid = false;
-			}
-		}
+        @Override
+        public void free() {
+            if (this.valid) {
+                DynamicVBO.this.free(this.sector);
+                this.valid = false;
+            }
+        }
 
-		@Override
-		public boolean isValid() {
-			return this.valid;
-		}
+        @Override
+        public boolean isValid() {
+            return this.valid;
+        }
 
-	}
+    }
 
 }
--- src/main/java/meldexun/nothirium/opengl/GLHelper.java
+++ src/main/java/meldexun/nothirium/opengl/GLHelper.java
@@ -2,46 +2,82 @@ package meldexun.nothirium.opengl;
 
 import java.nio.ByteBuffer;
 
+import org.lwjgl.opengl.ARBBufferStorage;
+import org.lwjgl.opengl.ARBCopyBuffer;
+import org.lwjgl.opengl.ARBDirectStateAccess;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL31;
+import org.lwjgl.opengl.GL44;
 import org.lwjgl.opengl.GL45;
 
+import meldexun.memoryutil.NIOBufferUtil;
 import meldexun.renderlib.util.GLUtil;
 
 public class GLHelper {
 
-	public static int growBuffer(int vbo, long oldSize, long newSize) {
-		if (GLUtil.CAPS.OpenGL45) {
-			int newVbo = GL45.glCreateBuffers();
-			GL45.glNamedBufferData(newVbo, newSize, GL15.GL_STREAM_DRAW);
-			GL45.glCopyNamedBufferSubData(vbo, newVbo, 0L, 0L, oldSize);
-			GL15.glDeleteBuffers(vbo);
-			return newVbo;
-		} else if (GLUtil.CAPS.OpenGL31) {
-			int newVbo = GL15.glGenBuffers();
-			GL15.glBindBuffer(GL31.GL_COPY_WRITE_BUFFER, newVbo);
-			GL15.glBufferData(GL31.GL_COPY_WRITE_BUFFER, newSize, GL15.GL_STREAM_DRAW);
-			GL15.glBindBuffer(GL31.GL_COPY_READ_BUFFER, vbo);
-			GL31.glCopyBufferSubData(GL31.GL_COPY_READ_BUFFER, GL31.GL_COPY_WRITE_BUFFER, 0L, 0L, oldSize);
-			GL15.glBindBuffer(GL31.GL_COPY_READ_BUFFER, 0);
-			GL15.glBindBuffer(GL31.GL_COPY_WRITE_BUFFER, 0);
-			GL15.glDeleteBuffers(vbo);
-			return newVbo;
-		} else {
-			int temp = GL15.glGenBuffers();
-			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, temp);
-			GL15.glBufferData(GL15.GL_ARRAY_BUFFER, oldSize, GL15.GL_STREAM_COPY);
-			ByteBuffer tempBuffer = GL15.glMapBuffer(GL15.GL_ARRAY_BUFFER, GL15.GL_READ_WRITE, oldSize, null);
-			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo);
-			GL15.glGetBufferSubData(GL15.GL_ARRAY_BUFFER, 0L, tempBuffer);
-			GL15.glBufferData(GL15.GL_ARRAY_BUFFER, newSize, GL15.GL_STREAM_DRAW);
-			GL15.glBufferSubData(GL15.GL_ARRAY_BUFFER, 0L, tempBuffer);
-			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, temp);
-			GL15.glUnmapBuffer(GL15.GL_ARRAY_BUFFER);
-			GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-			GL15.glDeleteBuffers(temp);
-			return vbo;
-		}
-	}
+    public static int growBuffer(int vbo, long oldSize, long newSize) {
+        if (GLUtil.CAPS.OpenGL45) {
+            int newVbo = GL45.glCreateBuffers();
+            GL45.glNamedBufferStorage(newVbo, newSize, GL44.GL_DYNAMIC_STORAGE_BIT);
+            GL45.glCopyNamedBufferSubData(vbo, newVbo, 0L, 0L, oldSize);
+            GL15.glDeleteBuffers(vbo);
+            return newVbo;
+        } else if (GLUtil.CAPS.GL_ARB_direct_state_access) {
+            int newVbo = ARBDirectStateAccess.glCreateBuffers();
+            ARBDirectStateAccess.glNamedBufferStorage(newVbo, newSize, GL44.GL_DYNAMIC_STORAGE_BIT);
+            ARBDirectStateAccess.glCopyNamedBufferSubData(vbo, newVbo, 0L, 0L, oldSize);
+            GL15.glDeleteBuffers(vbo);
+            return newVbo;
+        } else if (GLUtil.CAPS.OpenGL44) {
+            int newVbo = GL15.glGenBuffers();
+            GL15.glBindBuffer(GL31.GL_COPY_WRITE_BUFFER, newVbo);
+            GL44.glBufferStorage(GL31.GL_COPY_WRITE_BUFFER, newSize, GL44.GL_DYNAMIC_STORAGE_BIT);
+            GL15.glBindBuffer(GL31.GL_COPY_READ_BUFFER, vbo);
+            GL31.glCopyBufferSubData(GL31.GL_COPY_READ_BUFFER, GL31.GL_COPY_WRITE_BUFFER, 0L, 0L, oldSize);
+            GL15.glBindBuffer(GL31.GL_COPY_READ_BUFFER, 0);
+            GL15.glBindBuffer(GL31.GL_COPY_WRITE_BUFFER, 0);
+            GL15.glDeleteBuffers(vbo);
+            return newVbo;
+        } else if (GLUtil.CAPS.GL_ARB_buffer_storage) {
+            int newVbo = GL15.glGenBuffers();
+            GL15.glBindBuffer(GL31.GL_COPY_WRITE_BUFFER, newVbo);
+            ARBBufferStorage.glBufferStorage(GL31.GL_COPY_WRITE_BUFFER, newSize, ARBBufferStorage.GL_DYNAMIC_STORAGE_BIT);
+            GL15.glBindBuffer(GL31.GL_COPY_READ_BUFFER, vbo);
+            GL31.glCopyBufferSubData(GL31.GL_COPY_READ_BUFFER, GL31.GL_COPY_WRITE_BUFFER, 0L, 0L, oldSize);
+            GL15.glBindBuffer(GL31.GL_COPY_READ_BUFFER, 0);
+            GL15.glBindBuffer(GL31.GL_COPY_WRITE_BUFFER, 0);
+            GL15.glDeleteBuffers(vbo);
+            return newVbo;
+        } else if (GLUtil.CAPS.OpenGL31) {
+            int newVbo = GL15.glGenBuffers();
+            GL15.glBindBuffer(GL31.GL_COPY_WRITE_BUFFER, newVbo);
+            GL15.glBufferData(GL31.GL_COPY_WRITE_BUFFER, newSize, GL15.GL_DYNAMIC_COPY);
+            GL15.glBindBuffer(GL31.GL_COPY_READ_BUFFER, vbo);
+            GL31.glCopyBufferSubData(GL31.GL_COPY_READ_BUFFER, GL31.GL_COPY_WRITE_BUFFER, 0L, 0L, oldSize);
+            GL15.glBindBuffer(GL31.GL_COPY_READ_BUFFER, 0);
+            GL15.glBindBuffer(GL31.GL_COPY_WRITE_BUFFER, 0);
+            GL15.glDeleteBuffers(vbo);
+            return newVbo;
+        } else if (GLUtil.CAPS.GL_ARB_copy_buffer) {
+            int newVbo = GL15.glGenBuffers();
+            GL15.glBindBuffer(ARBCopyBuffer.GL_COPY_WRITE_BUFFER, newVbo);
+            GL15.glBufferData(ARBCopyBuffer.GL_COPY_WRITE_BUFFER, newSize, GL15.GL_DYNAMIC_COPY);
+            GL15.glBindBuffer(ARBCopyBuffer.GL_COPY_READ_BUFFER, vbo);
+            ARBCopyBuffer.glCopyBufferSubData(ARBCopyBuffer.GL_COPY_READ_BUFFER, ARBCopyBuffer.GL_COPY_WRITE_BUFFER, 0L, 0L, oldSize);
+            GL15.glBindBuffer(ARBCopyBuffer.GL_COPY_READ_BUFFER, 0);
+            GL15.glBindBuffer(ARBCopyBuffer.GL_COPY_WRITE_BUFFER, 0);
+            GL15.glDeleteBuffers(vbo);
+            return newVbo;
+        } else {
+            ByteBuffer temp = NIOBufferUtil.allocateByte(oldSize);
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo);
+            GL15.glGetBufferSubData(GL15.GL_ARRAY_BUFFER, 0L, temp);
+            GL15.glBufferData(GL15.GL_ARRAY_BUFFER, newSize, GL15.GL_DYNAMIC_COPY);
+            GL15.glBufferSubData(GL15.GL_ARRAY_BUFFER, 0L, temp);
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+            NIOBufferUtil.freeMemory(temp);
+            return vbo;
+        }
+    }
 
 }
